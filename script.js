aaae8db30c3901624d1bc8a7441ec7710de5a136
saltychocolat <86726264+saltychocolat@users.noreply.github.com>
2024-11-16 15:02:18 +0200

    Modular Js

--- script.js
+++ script.js
@@ -1,111 +1,57 @@
-let myLibrary = [];
-let id = 0;
-
-const content =document.querySelector(".content");
-const wrapper = document.querySelector(".wrapper");
-
-
-const dialog = document.querySelector("dialog");
-const form = document.querySelector("form");
-
-
-const newButton = document.querySelector("#new");
-const confirmButton = document.querySelector("#confirmButton");
-
-const title = document.querySelector("#title");
-const author = document.querySelector("#author");
-const pages = document.querySelector("#pages");
-
-
-function empty(element) {
-    element.innerHTML = ""; 
-}
-
-function Book(title, author, pages, read){
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
-}
-
-function createBook(){
-    empty(content)
-    for(let y=0;y<myLibrary.length;y++){
-        entry = myLibrary[y];
-
-        const card = document.createElement("div") ;
-        card.id =  entry.id;
-        card.classList.add("card")
-        
-        const removeButton = document.createElement("button");
-        removeButton.textContent = "Delete";
-        removeButton.classList.add("removeButton");
-
-        const title =document.createElement("div");
-        title.textContent = "Title: " + entry.book.title;
-    
-        const author =document.createElement("div");
-        author.textContent = "Author: " + entry.book.author;
-        
-        const pages =document.createElement("div");
-        pages.textContent = "Pages: " + entry.book.pages;
-
-
-        const hasRead =document.createElement("input");
-        const label = document.createElement("laber");
-        hasRead.setAttribute("type","checkbox");
-        hasRead.checked = entry.book.read;
-        label.textContent ="Read  ";
-
-        label.appendChild(hasRead);
-
-
-        card.appendChild(title);
-        card.appendChild(author);
-        card.appendChild(pages);
-        card.appendChild(label);
-        card.appendChild(removeButton)
-    
-        content.appendChild(card);
-        wrapper.appendChild(content);
-        document.body.appendChild(wrapper);
-    }
-
-
-
-}
-
-function addBookToLibrary(book) {
-    myLibrary.push({id,book})
-    createBook();
-    id++;
-}
-
-
-
-newButton.addEventListener('click',function(){
-    dialog.showModal();
-})
-
-confirmButton.addEventListener("click",function(event){
-    event.preventDefault();
-    let read = document.querySelector("#hasRead").checked;
-    const book = new Book(title.value,author.value,pages.value,read);
-    addBookToLibrary(book);
-
-    form.reset();
-    dialog.close()
-
-})
-
-document.addEventListener("click",function(event){
-    target = event.target;
-    if(target.className.includes("removeButton")){
-        target = target.parentElement;
-        for(let i=0;i<myLibrary.length;i++){
-            if(myLibrary[i].id==target.id)
-                myLibrary.splice(i,1);
+myLibrary = {
+    books:[{id:0,title:"Title",author:"Author",pages:234,hasRead:true}],
+    init:function(){
+        this.render();
+        this.getDom["confirmButton"].addEventListener("click",addBook);
+        this.getDom["newButton"].addEventListener("click",openDialog);
+    },
+    render:function(){
+        for(let i=0;i<this.books.length;i++){
+            book = this.books[i];
+            this.createCard(book);
         }
-        createBook();
-    }
-})
+    },
+    createCard:function(book){
+        getDom = this.getDom();
+        card = document.createElement("div");
+        card.classList.add("card");
+        for(key in book){
+            newDiv = this.createCategory(key,book);
+            card.appendChild(newDiv);
+        }
+        getDom["dialog"].appendChild(card);;
+        getDom["content"].appendChild(getDom["dialog"]);
+        getDom["wrapper"].appendChild(getDom["content"]);
+    },
+    createCategory(key,book){
+        div = document.createElement(key);
+        div.textContent = book[key];
+        return div;
+    },  
+    getDom:function(){
+        content =document.querySelector(".content");
+        wrapper = document.querySelector(".wrapper");
+        dialog = document.querySelector("dialog");
+        form = document.querySelector("form");
+        newButton = document.querySelector("#new");
+        confirmButton = document.querySelector("#confirmButton");
+        return {content:content,wrapper:wrapper,dialog:dialog,form:form,newButton:newButton,confirmButton:confirmButton};
+    },
+    getInput:function(){
+        title = document.querySelector("#title");
+        author = document.querySelector("#author");
+        pages = document.querySelector("#pages");
+        return {content:content,wrapper:wrapper,dialog:dialog,form:form,newButton:newButton,confirmButton:confirmButton,title:title,author:author,pages:pages};
+    },
+    addBook:function(event){
+        target = event.target;
+
+        books.push
+    },
+    openDialog:function(){
+        this.getDom["dialog"].show;
+    },
+
+}
+myLibrary.init();
+
